14321c8f7fb2c13bf869ed6a8dce64df37f6bb25
Martynas Kuliešius <martiss200@gmail.com>
2024-04-17 01:08:45 +0300

    Edit for network utils
    Added networkUtils class to get wifi adress

--- MinMaxApp/ESPController.cs
+++ MinMaxApp/ESPController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -8,12 +10,21 @@ namespace MinMaxApp
     {
         private readonly HttpClient _httpClient;
         private readonly string _esp32Url;
-
+        
         public ESPController(string esp32IpAddress)
         {
+            string wifiIpAddress = NetworkUtils.GetWifiIpAddress();
+            if (wifiIpAddress != null)
+            {
+                Debug.WriteLine("ABOBA Wi-Fi IP Address: " + wifiIpAddress);
+            }
+            else
+            {
+                Debug.WriteLine("ABOBA Failed to retrieve Wi-Fi IP address.");
+            }
             _httpClient = new HttpClient();
             //_esp32Url = $"http://{esp32IpAddress}/";
-            _esp32Url = "http://192.168.4.1";
+            _esp32Url = "http://"+wifiIpAddress;
         }
 
         public async Task<bool> DisplayNumber(int number)
@@ -21,14 +32,19 @@ namespace MinMaxApp
             try
             {
                 string endpoint = $"{_esp32Url}/show_number?number={number}";
+                //string endpoint = $"http://192.168.4.1/show_number?number={number}";
+            
                 HttpResponseMessage response = await _httpClient.GetAsync(endpoint);
 
                 // Check if the request was successful (status code 200)
+                Debug.WriteLine("ABOBA " + response);
                 return response.IsSuccessStatusCode;
+
+                
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                Debug.WriteLine($"ABOBA Error: {ex.Message}");
                 return false;
             }
         }
